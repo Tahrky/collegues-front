47fc71679d0a8f276abf21cb03972301c97aa0c3
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-26 17:04:25 +0200

    Mise en place d'un mock de Collegue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Collegue } from './models/Collegue';
+import { ColleguesMock } from './mock/collegues.mock';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,5 @@ import { Collegue } from './models/Collegue';
 
 export class AppComponent {
   title = 'collegues-front';
-  collegue1 = new Collegue ("1", "Nom", "Prenoms", "email@mail.com", "date", 
-  "https://s2.qwant.com/thumbr/0x0/5/f/3533c02d067aa13bf04c26eb3193a02d9d8f8c2f205b5b7a239a66eaddc99e/Popcorn_Large-Lid_shutterstock_71515753-1.jpg?u=https%3A%2F%2Fwww.nostalgiaproducts.com%2Fwp-content%2Fuploads%2F2017%2F08%2FPopcorn_Large-Lid_shutterstock_71515753-1.jpg&q=0&b=1&p=0&a=1");
-
-  collegue2 = new Collegue ("2", "Paul", "Gurpratap Singh", "email@mail.com", "1987-9-5", 
-  "https://s1.qwant.com/thumbr/0x0/b/2/f3aa8ab4c0cb8b3c89c7e5798ab1fb11e5d8e05339fa98cd101f292cd229e9/dieu.jpg?u=http%3A%2F%2Flesociologue.com%2Fwp-content%2Fuploads%2F2013%2F10%2Fdieu.jpg&q=0&b=1&p=0&a=1");
+  collegues:ColleguesMock = new ColleguesMock ();
 }
--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -1,7 +1,13 @@
 import { Collegue } from '../models/Collegue';
 
 export class ColleguesMock {
-    constructor (public col:Collegue) {
+    public collegue1 = new Collegue ("1", "Nom", "Prenoms", "email@mail.com", "date", 
+    "https://s2.qwant.com/thumbr/0x0/5/f/3533c02d067aa13bf04c26eb3193a02d9d8f8c2f205b5b7a239a66eaddc99e/Popcorn_Large-Lid_shutterstock_71515753-1.jpg?u=https%3A%2F%2Fwww.nostalgiaproducts.com%2Fwp-content%2Fuploads%2F2017%2F08%2FPopcorn_Large-Lid_shutterstock_71515753-1.jpg&q=0&b=1&p=0&a=1");
+
+    public collegue2 = new Collegue ("2", "Paul", "Gurpratap Singh", "email@mail.com", "1987-9-5", 
+    "https://s1.qwant.com/thumbr/0x0/b/2/f3aa8ab4c0cb8b3c89c7e5798ab1fb11e5d8e05339fa98cd101f292cd229e9/dieu.jpg?u=http%3A%2F%2Flesociologue.com%2Fwp-content%2Fuploads%2F2013%2F10%2Fdieu.jpg&q=0&b=1&p=0&a=1");
+
+    constructor () {
 
     }
 }
